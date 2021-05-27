e616072d93463f99d0cdb5586ed5d2ead16f9172
cbrzn <cesarbrazon10@gmail.com>
2021-05-27 14:22:26 +0200

    chore: remove all stopped containers

--- deploy_to_aws.sh
+++ deploy_to_aws.sh
@@ -4,6 +4,7 @@ echo 'Starting to Deploy...'
 ssh ec2-user@18.221.73.93 "docker image prune -f 
         cd web3hub 
         docker-compose down
+        docker container rm $(docker container ls -a)
         git fetch fork
         git reset --hard fork/dockerization  &&  echo 'You are doing well'
         docker-compose build && docker-compose up -d
