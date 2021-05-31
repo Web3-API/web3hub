9189726a180c11191a387656a0084b8d5aac64ea
cbrzn <cesarbrazon10@gmail.com>
2021-05-31 17:56:24 +0200

    chore: trying to make thread js work

--- packages/ui/src/pages/_app.tsx
+++ packages/ui/src/pages/_app.tsx
@@ -24,6 +24,7 @@ function StatefulApp({pageProps, Component}) {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Poppins:wght@100;200;300;400;500;600;700;800;900&family=Istok+Web:wght@400;700&&display=swap"
             rel="stylesheet"
           ></link>
+          <script type="text/javascript" src="thread.js"/>
         </Head>
         <SWRConfig value={swrOptions}>
           <Component {...pageProps} />
