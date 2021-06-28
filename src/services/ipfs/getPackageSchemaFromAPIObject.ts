75dc5208b70c04ed08d9ef73dc549f7547de238b
cbrzn <cesarbrazon10@gmail.com>
2021-06-29 01:29:39 +0200

    chore: remove console log

--- src/services/ipfs/getPackageSchemaFromAPIObject.ts
+++ src/services/ipfs/getPackageSchemaFromAPIObject.ts
@@ -3,7 +3,6 @@ import axios from 'axios'
 import { APIData } from '../../hooks/ens/useGetAPIfromENS'
 
 export default async function getPackageSchema(api: APIData): Promise<string> {
-  console.log(api)
   let schemaResponse = await axios.get(
     `${cloudFlareGateway}${api.locationUri}/schema.graphql`,
   )
