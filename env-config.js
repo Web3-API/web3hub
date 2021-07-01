1893ebbd1d850c855d52f68d7d4ad70527c58f29
cbrzn <cesarbrazon10@gmail.com>
2021-07-01 19:45:11 +0200

    fix: added gh client keys again

--- env-config.js
+++ env-config.js
@@ -1,5 +1,7 @@
 module.exports = {
   WEB3API_THREAD_PATH: '../../thread.js',
-  DATABASE_URL: 'postgres://qlvnktjkyihemu:5fc213811f2851cbe463540aac25d039e0121dfca47cfc72a97a3059f244332c@ec2-54-90-211-192.compute-1.amazonaws.com:5432/d7htnshjgn90i3',
-  ETH_RPC_NODE: 'https://ropsten.infura.io/v3/b76cba91dc954ceebff27244923224b1'
+  DATABASE_URL: process.env.DATABASE_URL,
+  ETH_RPC_NODE: 'https://ropsten.infura.io/v3/b76cba91dc954ceebff27244923224b1',
+  GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+  GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET
 }
